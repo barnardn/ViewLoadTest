8c63030bf9faa2bbd57020a34f431eb99647b85a
Norm Barnard <barnard@clamdango.com>
2015-11-29 16:29:50 -0500

    Initial Commit

---
+++ ViewLoadTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewLoadTest
+//
+//  Created by Norm Barnard on 11/29/15.
+//  Copyright © 2015 Clamdango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewLoadTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ViewLoadTest
+//
+//  Created by Norm Barnard on 11/29/15.
+//  Copyright © 2015 Clamdango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
